da313063436f23994307805f2508f7acd77efc51
q1110 <zeratuliche@gmail.com>
2026-02-12 14:21:57 +0300

    add funtion create_user

--- src/database.rs
+++ src/database.rs
@@ -16,4 +16,14 @@ impl Database {
             .context("error while trying connect to database")?;
         Ok(Database { pg_pool: pg_pool })
     }
+    pub async fn create_user(&self, telegram_id: i32) -> anyhow::Result<()> {
+        sqlx::query!(
+            "INSERT INTO users (telegram_id, lvl, exp) VALUES ($1, 0, 0)",
+            telegram_id
+        )
+        .execute(&self.pg_pool)
+        .await
+        .context("error while create user")?;
+        Ok(())
+    }
 }
